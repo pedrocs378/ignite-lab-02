20c26349215d22f6f7ed74ce850eff7a64b7c56d
Pedro <pedrocs378@gmail.com>
2022-06-23 16:20:13 -0300

    feat: cria header

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,8 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-type Lesson = {
-  id: string
-  title: string
-}
-
-type Query = {
-  lessons: Lesson[]
-}
+import { Event } from './pages/Event'
 
 function App() {
-  const { data } = useQuery<Query>(GET_LESSONS_QUERY)
-
-  console.log(data)
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <Event />
   )
 }
 
